2b9ae878e79623126124076235e6c9a60cc1685a
praveent071 <141566802+praveent071@users.noreply.github.com>
2023-08-14 19:08:10 +0530

    Add files via upload

---
+++ Nullcheck/src/NullCheck.java
@@ -0,0 +1,21 @@
+
+public class NullCheck {
+	
+	public static void main(String[] arg) {
+		 
+		NullCheck n=new NullCheck();
+		n.m(null);
+	}
+		
+		void m(Object obj) {
+			
+			System.out.println("222");
+		}
+	    void m(String str) {
+			
+			System.out.println("111");
+		}
+		
+
+	}
+
